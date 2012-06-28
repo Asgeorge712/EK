c53f41844a1636652580954753f732fab92a2079
Paul George <paul@paulgeorge.com>
2012-06-28 18:24:21 -0400

    Initial Commit after git hell

---
+++ EK4Andriod/src/com/paulgeorge/ek/EyeKeeperAppActivity.java
@@ -0,0 +1,13 @@
+package com.paulgeorge.ek;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EyeKeeperAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
